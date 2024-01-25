766d04eb3234517679dc9c1f81013fe6f51f3717
C5D2 <yoakec5d2@naver.com>
2024-01-26 01:29:37 +0900

    워들 게임 완성

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
-answer='TRAIN'
+answer='APPLE'
 
 @app.get('/answer')
 def get_answer():
-    return {'answer':answer}
+    return answer
 
 app.mount("/", StaticFiles(directory="static",html=True), name="static")
